e38ff22a15b6f2e4ff073e70d51adbd14c73e1cd
Olep8h <panov@student.vspj.cz>
2022-11-02 23:57:23 +0200

    modal window uebermarginal

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React from 'react';
 import './index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+    const [open, setOpen] = React.useState(false);
+    return (
+        <div className="App">
+            <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Open window</button>
+            {open && (
+                <div className="overlay">
+                    <div className="modal">
+                        <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+                            <title/>
+                            <path
+                                d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
+                        </svg>
+                        <img src="https://media.tenor.com/q9YVSNL8KtgAAAAd/uberdance1-%D1%83%D0%B1%D0%B5%D1%80%D0%BC%D0%B0%D1%80%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB.gif"/>
+                    </div>
+                </div>
+            )}
         </div>
-      </div> */}
-    </div>
-  );
+    );
 }
 
 export default App;
+

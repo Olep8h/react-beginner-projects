4bb6a961941a0a6c39d6c7749d315daae47d830b
Olep8h <panov@student.vspj.cz>
2022-11-05 17:05:49 +0200

    My photo collection

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import React from 'react';
 import {Collection} from "./Collection";
 import './index.scss';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD5NbJn0HyhAqcQPSfuN3kKcJQ0YaKrlTE",
+    authDomain: "my-photo-collection-1db46.firebaseapp.com",
+    projectId: "my-photo-collection-1db46",
+    storageBucket: "my-photo-collection-1db46.appspot.com",
+    messagingSenderId: "269985181744",
+    appId: "1:269985181744:web:cfb69ca64179ea4295fdf5",
+    measurementId: "G-Y5XV72VD6K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 function App() {
   return (

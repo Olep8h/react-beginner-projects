7f675e8684952a0a80214fd4e431ecba9562cb55
Olep8h <panov@student.vspj.cz>
2022-11-05 04:36:19 +0200

    Currency Converter

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+    const [fromCurrency, setFromCurrency] = React.useState('CZK');
+    const [toCurrency, setToCurrency] = React.useState('USD');
+    const [rates, setRates] = React.useState({});
+
+    React.useEffect(() => {
+        fetch('https://cdn.cur.su/api/latest.json')
+            .then((res) => res.json())
+            .then((json) => {
+                setRates(json.rates);
+            })
+            .catch((err) => {
+                console.warn(err);
+                alert('Failed to get information')
+            });
+    }, []);
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block value={0} currency="fromCurrency" onChangeCurrency={setFromCurrency} />
+        <Block value={0} currency="toCurrency" onChangeCurrency={setToCurrency} />
     </div>
   );
 }

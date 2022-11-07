cb83658feddf8ccdbe2b4e36e5e9c5fb5f5220b0
Olep8h <panov@student.vspj.cz>
2022-11-08 00:32:59 +0200

    + Different folders

--- src/App.js
+++ src/App.js
@@ -26,6 +26,21 @@ const analytics = getAnalytics(app);
 
 
 function App() {
+    const [collections, setCollections] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('https://6365b9d5046eddf1baf1db58.mockapi.io/inspired/v1/Photos')
+            .then((res) => res.json())
+            .then((json) => {
+                setCollections(json);
+            })
+            .catch(err => {
+                console.warn(err);
+                alert(`Error while getting data`);
+            });
+    }, []);
+
+
   return (
     <div className="App">
       <h1>My photo collection</h1>
@@ -40,15 +55,12 @@ function App() {
         <input className="search-input" placeholder="Search by name" />
       </div>
       <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
+            {collections.map((obj) => (
+                <Collection
+                name={obj.name}
+                images={obj.photos}
+                />
+                ))}
       </div>
       <ul className="pagination">
         <li>1</li>
